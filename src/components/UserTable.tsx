21fff6325d6ce8d6745c4473e186e0186df8feee
Sebastian Garas <sebastian.garas66@gmail.com>
2024-09-09 18:28:40 +0200

    added a information when table is empty

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,8 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { getUsers, setFilter } from '../features/users/usersSlice';
 import SearchInput from './SearchInput';
 import styles from '../styles/UserTable.module.css';
+
+interface Filter {
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+}
 
 const UserTable = () => {
     const dispatch = useAppDispatch();
@@ -12,18 +19,21 @@ const UserTable = () => {
         dispatch(getUsers());
     }, [dispatch]);
 
-    const handleFilterChange = (field: string, value: string) => {
-        dispatch(setFilter({ field: field as keyof typeof filter, value }));
+    const handleFilterChange = (field: keyof Filter, value: string) => {
+        dispatch(setFilter({ field, value }));
     };
 
-    const filteredUsers = users.filter((user) =>
-        Object.keys(filter).every((key) =>
-            user[key as keyof typeof filter].toLowerCase().includes(filter[key as keyof typeof filter].toLowerCase())
-        )
-    );
+    const filteredUsers = useMemo(() => {
+        if (!users.length) return [];
+        return users.filter((user) =>
+            Object.keys(filter).every((key) =>
+                user[key as keyof Filter].toLowerCase().includes((filter as { [key: string]: string })[key].toLowerCase())
+            )
+        );
+    }, [users, filter]);
 
-    if (!users.length) {
-        return null;
+    if (!filteredUsers.length) {
+        return <p>No users found.</p>;
     }
 
     return (
@@ -31,34 +41,14 @@ const UserTable = () => {
             <table className={styles.table}>
                 <thead>
                     <tr>
-                        <th>
-                            Name
-                            <SearchInput
-                                value={filter.name}
-                                onChange={(e) => handleFilterChange('name', e.target.value)}
+                        {(['name', 'username', 'email', 'phone'] as const).map((field) => (
+                            <TableHeaderCell
+                                key={field}
+                                field={field}
+                                value={filter[field]}
+                                onChange={handleFilterChange}
                             />
-                        </th>
-                        <th>
-                            Username
-                            <SearchInput
-                                value={filter.username}
-                                onChange={(e) => handleFilterChange('username', e.target.value)}
-                            />
-                        </th>
-                        <th>
-                            Email
-                            <SearchInput
-                                value={filter.email}
-                                onChange={(e) => handleFilterChange('email', e.target.value)}
-                            />
-                        </th>
-                        <th>
-                            Phone
-                            <SearchInput
-                                value={filter.phone}
-                                onChange={(e) => handleFilterChange('phone', e.target.value)}
-                            />
-                        </th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
@@ -73,6 +63,21 @@ const UserTable = () => {
                 </tbody>
             </table>
         </div>
+    );
+};
+
+interface TableHeaderCellProps {
+    field: keyof Filter;
+    value: string;
+    onChange: (field: keyof Filter, value: string) => void;
+}
+
+const TableHeaderCell = ({ field, value, onChange }: TableHeaderCellProps) => {
+    return (
+        <th>
+            {field.charAt(0).toUpperCase() + field.slice(1)}
+            <SearchInput value={value} onChange={(e) => onChange(field, e.target.value)} />
+        </th>
     );
 };
 
